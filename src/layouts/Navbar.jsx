91f6c7d0339bda602f7f7442980fb3dd89528577
Ginachiko1998 <soniagina669@gmail.com>
2023-09-13 18:06:18 +0700

    change navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,7 +4,6 @@ import logo from "../assets/logo.png";
 import navbarBg from "../assets/home-bg.jpg";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => (
   <div className="">
     <div
@@ -23,30 +22,34 @@ const Navbar = () => (
         <div className="text-sm lg:flex-grow"></div>
         <div className="lg:mt-0 mt-5">
           <div className="lg:hidden">
-            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900 mt-2">
+            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg  mt-2">
               <Link to={"/about"}>ABOUT</Link>
             </div>
-            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900 mt-2">
+            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg mt-2">
               <Link to={"/pricing"}>PRICING</Link>
             </div>
-            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900 mt-2">
+            <div className="block lg:text-black text-white font-bold lg:text-3xl text-lg mt-2">
               <Link to={"/contact"}>CONTACT</Link>
             </div>
-            <div className={`block px-12 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-white lg:border-blue-400 border-teal hover:border-transparent hover:text-white hover:bg-blue-900 mt-4`}>
+            <div
+              className={`block px-12 text-center w-32 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-blue-400 lg:border-blue-400 border-teal mt-4`}
+            >
               <Link to={"/login"}>LOGIN</Link>
             </div>
           </div>
           <div className="hidden lg:block">
-            <div className="items-center mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900 mr-12">
+            <div className="items-center mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg mr-12">
               <Link to={"/about"}>ABOUT</Link>
             </div>
-            <div className="mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900 mr-12">
+            <div className="mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg mr-12">
               <Link to={"/pricing"}>PRICING</Link>
             </div>
-            <div className="mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg hover:text-blue-900">
+            <div className="mt-4 lg:inline-block lg:mt-0 lg:text-black text-white font-bold lg:text-3xl text-lg">
               <Link to={"/contact"}>CONTACT</Link>
             </div>
-            <div className={`inline-block px-12 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-white lg:border-blue-400 border-teal hover:border-transparent hover:text-white hover:bg-blue-900 mt-4 lg:mt-0 lg:ml-12 ml-0`}>
+            <div
+              className={`inline-block px-12 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-blue-400 lg:border-blue-400 mt-4 lg:mt-0 lg:ml-12 ml-0`}
+            >
               <Link to={"/login"}>LOGIN</Link>
             </div>
           </div>
@@ -57,4 +60,3 @@ const Navbar = () => (
 );
 
 export default Navbar;
-
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,51 +1,46 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // Import useNavigate dari react-router-dom
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username, setUsername] = useState(""); // Tambahkan state untuk username
-  const [status, setStatus] = useState("mahasiswa"); // Tambahkan state untuk status
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Inisialisasi useNavigate
 
-  const handleRegister = () => {
-    if (
-      !email.includes("@") ||
-      !email.includes(".com") ||
-      password.length < 6 ||
-      !/^[a-zA-Z\s]*$/.test(username) // Validasi hanya menerima teks untuk username
-    ) {
-      setErrorMessage("Email, password, atau username salah.");
-      navigate("/errorpage");
+  const handleLogin = () => {
+    if (!email.includes("@") || !email.includes(".com") || password.length < 6) {
+      setErrorMessage("Email atau password salah.");
+      navigate("/errorpage"); // Arahkan ke halaman error jika login gagal
     } else {
       setErrorMessage("");
-      setSuccessMessage("Successful register");
-      navigate("/login");
+      setSuccessMessage("Welcome in the web!");
+      navigate("/login"); // Arahkan ke halaman beranda jika berhasil login
     }
   };
 
   return (
     <div>
-      <div className="h-screen overflow-hidden flex items-center justify-center">
+      <div
+        className="h-screen overflow-hidden flex items-center justify-center"
+      >
         <div className="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl border border-gray-300">
           <form className="p-12 md:p-24" action="/">
             <h2 className="text-center font-bold lg:text-4xl text-2xl mb-10">
-              REGISTER
+              Register
             </h2>
             <div className="mt-3 mb-2">
-              <label htmlFor="username" className="mr-2">
+              <label htmlFor="email" className="mr-2">
                 Username
               </label>
             </div>
             <input
               type="text"
-              id="username"
+              id="email"
               className="bg-gray-200 pl-5 py-2 md:py-4 focus:outline-none w-full"
               placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <div className="mt-3 mb-2">
               <label htmlFor="email" className="mr-2">
@@ -73,21 +68,6 @@ const Register = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <div className="mt-3 mb-2">
-              <label htmlFor="status" className="mr-2">
-                Status
-              </label>
-            </div>
-            <select
-              id="status"
-              className="bg-gray-200 pl-2 py-2 md:py-4 focus:outline-none w-full"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-            >
-              <option value="mahasiswa">Mahasiswa</option>
-              <option value="pelajar">Pelajar</option>
-              <option value="pekerja">Pekerja</option>
-            </select>
             {errorMessage && <p className="text-red-500">{errorMessage}</p>}
             {successMessage && (
               <p className="text-green-500">{successMessage}</p>
@@ -95,13 +75,13 @@ const Register = () => {
             <button
               className="font-medium p-2 md:p-4 text-white uppercase w-full mt-4"
               style={{ backgroundColor: "#21BFF7" }}
-              onClick={handleRegister}
+              onClick={handleLogin}
             >
               Register
             </button>
           </form>
           <p className="text-center">
-            Have an account?{" "}
+            Have account?{" "}
             <a href="/login" className="text-blue-500">
               Login Here
             </a>
